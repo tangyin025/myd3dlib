ebf64783b84e460f7226edccbee568853ffe585e
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2011-08-16 15:29:29 +0000

    git-svn-id: svn://127.0.0.1/repos/trunk@48 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- myd3dlib/include/myMath.h
+++ myd3dlib/include/myMath.h
@@ -5,6 +5,10 @@
 
 namespace my
 {
+	class Vector4;
+
+	class Matrix4;
+
 	class Vector2
 	{
 	public:
@@ -119,6 +123,46 @@ namespace my
 			y /= scaler;
 			return *this;
 		}
+
+	public:
+		float dot(const Vector2 & rhs) const
+		{
+			return x * rhs.x + y * rhs.y;
+		}
+
+		float length(void) const
+		{
+			return sqrt(lengthSq());
+		}
+
+		float lengthSq(void) const
+		{
+			return x * x + y * y;
+		}
+
+		Vector2 lerp(const Vector2 & rhs, float s) const
+		{
+			return Vector2(
+				x + s * (rhs.x - x),
+				y + s * (rhs.y - y));
+		}
+
+		Vector2 normalize(void) const
+		{
+			float l = length();
+
+			return Vector2(x / l, y / l);
+		}
+
+		Vector2 & normalizeSelf(void)
+		{
+			float l = length();
+			x = x / l;
+			y = y / l;
+			return *this;
+		}
+
+		Vector4 transform(const Matrix4 & m) const;
 
 	public:
 		static const Vector2 zero;
@@ -252,6 +296,56 @@ namespace my
 			z /= scaler;
 			return *this;
 		}
+
+	public:
+		Vector3 cross(const Vector3 & rhs) const
+		{
+			return Vector3(
+				y * rhs.z - z * rhs.y,
+				z * rhs.x - x * rhs.z,
+				x * rhs.y - y * rhs.x);
+		}
+
+		float dot(const Vector3 & rhs) const
+		{
+			return x * rhs.x + y * rhs.y + z * rhs.z;
+		}
+
+		float length(void) const
+		{
+			return sqrt(lengthSq());
+		}
+
+		float lengthSq(void) const
+		{
+			return x * x + y * y + z * z;
+		}
+
+		Vector3 lerp(const Vector3 & rhs, float s)
+		{
+			return Vector3(
+				x + s * (rhs.x - x),
+				y + s * (rhs.y - y),
+				z + s * (rhs.z - z));
+		}
+
+		Vector3 normalize(void) const
+		{
+			float l = length();
+
+			return Vector3(x / l, y / l, z / l);
+		}
+
+		Vector3 & normalizeSelf(void)
+		{
+			float l = length();
+			x = x / l;
+			y = y / l;
+			z = z / l;
+			return *this;
+		}
+
+		Vector4 transform(const Matrix4 & m) const;
 
 	public:
 		static const Vector3 zero;
@@ -407,6 +501,50 @@ namespace my
 		}
 
 	public:
+		float dot(const Vector4 & rhs) const
+		{
+			return x * rhs.x + y * rhs.y + z * rhs.z + w * rhs.w;
+		}
+
+		float length(void) const
+		{
+			return sqrt(lengthSq());
+		}
+
+		float lengthSq(void) const
+		{
+			return x * x + y * y + z * z + w * w;
+		}
+
+		Vector4 lerp(const Vector4 & rhs, float s)
+		{
+			return Vector4(
+				x + s * (rhs.x - x),
+				y + s * (rhs.y - y),
+				z + s * (rhs.z - z),
+				w + s * (rhs.w - w));
+		}
+
+		Vector4 normalize(void) const
+		{
+			float l = length();
+
+			return Vector4(x / l, y / l, z / l, w / l);
+		}
+
+		Vector4 & normalizeSelf(void)
+		{
+			float l = length();
+			x = x / l;
+			y = y / l;
+			z = z / l;
+			w = w / l;
+			return *this;
+		}
+
+		Vector4 transform(const Matrix4 & m) const;
+
+	public:
 		static const Vector4 zero;
 
 		static const Vector4 unitX;
@@ -510,7 +648,7 @@ namespace my
 		Matrix4 operator / (const Matrix4 & rhs) const
 		{
 			Matrix4 ret;
-			return *this * rhs.Inverse();
+			return *this * rhs.inverse();
 		}
 
 		Matrix4 operator / (float scaler) const
@@ -587,14 +725,183 @@ namespace my
 		}
 
 	public:
+		float a11(void) const
+		{
+			return _22 * _33 * _44
+				 + _23 * _34 * _42
+				 + _24 * _32 * _43
+				 - _22 * _34 * _43
+				 - _23 * _32 * _44
+				 - _24 * _33 * _42;
+		}
+
+		float a12(void) const
+		{
+			return _23 * _34 * _41
+				 + _24 * _31 * _43
+				 + _21 * _33 * _44
+				 - _23 * _31 * _44
+				 - _24 * _33 * _41
+				 - _21 * _34 * _43;
+		}
+
+		float a13(void) const
+		{
+			return _24 * _31 * _42
+				 + _21 * _32 * _44
+				 + _22 * _34 * _41
+				 - _24 * _32 * _41
+				 - _21 * _34 * _42
+				 - _22 * _31 * _44;
+		}
+
+		float a14(void) const
+		{
+			return _21 * _32 * _43
+				 + _22 * _33 * _41
+				 + _23 * _31 * _42
+				 - _21 * _33 * _42
+				 - _22 * _31 * _43
+				 - _23 * _32 * _41;
+		}
+
+		float a21(void) const
+		{
+			return _32 * _43 * _14
+				 + _33 * _44 * _12
+				 + _34 * _42 * _13
+				 - _32 * _44 * _13
+				 - _33 * _42 * _14
+				 - _34 * _43 * _12;
+		}
+
+		float a22(void) const
+		{
+			return _33 * _44 * _11
+				 + _34 * _41 * _13
+				 + _31 * _43 * _14
+				 - _33 * _41 * _14
+				 - _34 * _43 * _11
+				 - _31 * _44 * _13;
+		}
+
+		float a23(void) const
+		{
+			return _34 * _41 * _12
+				 + _31 * _42 * _14
+				 + _32 * _44 * _11
+				 - _34 * _42 * _11
+				 - _31 * _44 * _12
+				 - _32 * _41 * _14;
+		}
+
+		float a24(void) const
+		{
+			return _31 * _42 * _13
+				 + _32 * _43 * _11
+				 + _33 * _41 * _12
+				 - _31 * _43 * _12
+				 - _32 * _41 * _13
+				 - _33 * _42 * _11;
+		}
+
+		float a31(void) const
+		{
+			return _42 * _13 * _24
+				 + _43 * _14 * _22
+				 + _44 * _12 * _23
+				 - _42 * _14 * _23
+				 - _43 * _12 * _24
+				 - _44 * _13 * _22;
+		}
+
+		float a32(void) const
+		{
+			return _43 * _14 * _21
+				 + _44 * _11 * _23
+				 + _41 * _13 * _24
+				 - _43 * _11 * _24
+				 - _44 * _13 * _21
+				 - _41 * _14 * _23;
+		}
+
+		float a33(void) const
+		{
+			return _44 * _11 * _22
+				 + _41 * _12 * _24
+				 + _42 * _14 * _21
+				 - _44 * _12 * _21
+				 - _41 * _14 * _22
+				 - _42 * _11 * _24;
+		}
+
+		float a34(void) const
+		{
+			return _41 * _12 * _23
+				 + _42 * _13 * _21
+				 + _43 * _11 * _22
+				 - _41 * _13 * _22
+				 - _42 * _11 * _23
+				 - _43 * _12 * _21;
+		}
+
+		float a41(void) const
+		{
+			return _12 * _23 * _34
+				 + _13 * _24 * _32
+				 + _14 * _22 * _33
+				 - _12 * _24 * _33
+				 - _13 * _22 * _34
+				 - _14 * _23 * _32;
+		}
+
+		float a42(void) const
+		{
+			return _13 * _24 * _31
+				 + _14 * _21 * _33
+				 + _11 * _23 * _34
+				 - _13 * _21 * _34
+				 - _14 * _23 * _31
+				 - _11 * _24 * _33;
+		}
+
+		float a43(void) const
+		{
+			return _14 * _21 * _32
+				 + _11 * _22 * _34
+				 + _12 * _24 * _31
+				 - _14 * _22 * _31
+				 - _11 * _24 * _32
+				 - _12 * _21 * _34;
+		}
+
+		float a44(void) const
+		{
+			return _11 * _22 * _33
+				 + _12 * _23 * _31
+				 + _13 * _21 * _32
+				 - _11 * _23 * _32
+				 - _12 * _21 * _33
+				 - _13 * _22 * _31;
+		}
+
+		Matrix4 adjoint(void) const
+		{
+			return Matrix4(
+				 a11(), -a21(),  a31(), -a41(),
+				-a12(),  a22(), -a32(),  a42(),
+				 a13(), -a23(),  a33(), -a43(),
+				-a14(),  a24(), -a34(),  a44());
+		}
+
 		//void Decompose(Vector3 & outScale, Quaternion & outRotation, Vector3 & outTranslation)
 		//{
-		//	D3DXMatrixDecompose((D3DXVECTOR3 *)&outScale, (D3DXQUATERNION *)&outRotation, (D3DXVECTOR3 *)&outTranslation, (D3DXMATRIX *)this);
+		//	D3DXMatrixDecompose((D3DXVECTOR3 *)&outScale, (D3DXVECTOR3 *)&outRotation, (D3DXVECTOR3 *)&outTranslation, (D3DXMATRIX *)this);
 		//}
 
-		float Determinant(void) const
+		float determinant(void) const
 		{
-			return D3DXMatrixDeterminant((D3DXMATRIX *)this);
+			return _11 * a11() - _12 * a12() + _13 * a13() - _14 * a14();
 		}
 
 		Matrix4 & SetIdentity(void)
@@ -603,12 +910,9 @@ namespace my
 			return *this;
 		}
 
-		Matrix4 Inverse(void) const
+		Matrix4 inverse(void) const
 		{
-			Matrix4 ret;
-			float det;
-			D3DXMatrixInverse((D3DXMATRIX *)&ret, &det, (D3DXMATRIX *)this);
-			return ret;
+			return adjoint() / determinant();
 		}
 
 		Matrix4 & SetLookAtLH(const Vector3 & eye, const Vector3 & at, const Vector3 & up)
@@ -617,16 +921,14 @@ namespace my
 			return *this;
 		}
 
-		Matrix4 Multiply(const Matrix4 & rhs) const
+		Matrix4 multiply(const Matrix4 & rhs) const
 		{
 			return *this * rhs;
 		}
 
-		Matrix4 MultiplyTranspose(const Matrix4 & rhs) const
+		Matrix4 multiplyTranspose(const Matrix4 & rhs) const
 		{
-			Matrix4 ret;
-			D3DXMatrixMultiplyTranspose((D3DXMATRIX *)&ret, (D3DXMATRIX *)this, (D3DXMATRIX *)&rhs);
-			return ret;
+			return multiply(rhs).transpose();
 		}
 
 		Matrix4 & SetLookAtRH(const Vector3 & eye, const Vector3 & at, const Vector3 & up)
@@ -775,7 +1077,7 @@ namespace my
 			return *this;
 		}
 
-		Matrix4 Transpose(void)
+		Matrix4 transpose(void)
 		{
 			Matrix4 ret;
 			D3DXMatrixTranspose((D3DXMATRIX *)&ret, (D3DXMATRIX *)this);
@@ -783,7 +1085,7 @@ namespace my
 		}
 
 	public:
-		Matrix4 Scaling(const Vector3 & scaling) const
+		Matrix4 scaling(const Vector3 & scaling) const
 		{
 			return Matrix4(
 				_11 * scaling.x, _12 * scaling.y, _13 * scaling.z, _14 * 1,
@@ -792,7 +1094,7 @@ namespace my
 				_41 * scaling.x, _42 * scaling.y, _43 * scaling.z, _44 * 1);
 		}
 
-		Matrix4 & ScalingSelf(const Vector3 & scaling)
+		Matrix4 & scalingSelf(const Vector3 & scaling)
 		{
 			_11 = _11 * scaling.x; _12 = _12 * scaling.y; _13 = _13 * scaling.z; _14 = _14 * 1;
 			_21 = _21 * scaling.x; _22 = _22 * scaling.y; _23 = _23 * scaling.z; _24 = _24 * 1;
@@ -801,58 +1103,58 @@ namespace my
 			return *this;
 		}
 
-		Matrix4 RotationX(float angle) const
+		Matrix4 rotationX(float angle) const
 		{
-			float cosangle = cos(angle);
-			float sinangle = sin(angle);
+			float c = cos(angle);
+			float s = sin(angle);
 
 			return Matrix4(
-				_11, _12 * cosangle - _13 * sinangle, _12 * sinangle + _13 * cosangle, _14,
-				_21, _22 * cosangle - _23 * sinangle, _22 * sinangle + _23 * cosangle, _24,
-				_31, _32 * cosangle - _33 * sinangle, _32 * sinangle + _33 * cosangle, _34,
-				_41, _42 * cosangle - _43 * sinangle, _42 * sinangle + _43 * cosangle, _44);
+				_11, _12 * c - _13 * s, _12 * s + _13 * c, _14,
+				_21, _22 * c - _23 * s, _22 * s + _23 * c, _24,
+				_31, _32 * c - _33 * s, _32 * s + _33 * c, _34,
+				_41, _42 * c - _43 * s, _42 * s + _43 * c, _44);
 		}
 
-		Matrix4 & RotationXSelf(float angle)
+		Matrix4 & rotationXSelf(float angle)
 		{
-			return *this = RotationX(angle);
+			return *this = rotationX(angle);
 		}
 
-		Matrix4 RotationY(float angle) const
+		Matrix4 rotationY(float angle) const
 		{
-			float cosangle = cos(angle);
-			float sinangle = sin(angle);
+			float c = cos(angle);
+			float s = sin(angle);
 
 			return Matrix4(
-				_11 * cosangle + _13 * sinangle, _12, _13 * sinangle - _11 * cosangle, 14,
-				_21 * cosangle + _23 * sinangle, _22, _23 * sinangle - _21 * cosangle, 14,
-				_31 * cosangle + _33 * sinangle, _32, _33 * sinangle - _31 * cosangle, 14,
-				_41 * cosangle + _43 * sinangle, _42, _43 * sinangle - _41 * cosangle, 14);
+				_11 * c + _13 * s, _12, _13 * s - _11 * c, 14,
+				_21 * c + _23 * s, _22, _23 * s - _21 * c, 14,
+				_31 * c + _33 * s, _32, _33 * s - _31 * c, 14,
+				_41 * c + _43 * s, _42, _43 * s - _41 * c, 14);
 		}
 
-		Matrix4 & RotationYSelf(float angle)
+		Matrix4 & rotationYSelf(float angle)
 		{
-			return *this = RotationY(angle);
+			return *this = rotationY(angle);
 		}
 
-		Matrix4 RotationZ(float angle) const
+		Matrix4 rotationZ(float angle) const
 		{
-			float cosangle = cos(angle);
-			float sinangle = sin(angle);
+			float c = cos(angle);
+			float s = sin(angle);
 
 			return Matrix4(
-				_11 * cosangle - _12 * sinangle, _11 * sinangle + _12 * cosangle, _13, _14,
-				_21 * cosangle - _22 * sinangle, _21 * sinangle + _22 * cosangle, _23, _24,
-				_31 * cosangle - _32 * sinangle, _31 * sinangle + _32 * cosangle, _33, _34,
-				_41 * cosangle - _42 * sinangle, _41 * sinangle + _42 * cosangle, _43, _44);
+				_11 * c - _12 * s, _11 * s + _12 * c, _13, _14,
+				_21 * c - _22 * s, _21 * s + _22 * c, _23, _24,
+				_31 * c - _32 * s, _31 * s + _32 * c, _33, _34,
+				_41 * c - _42 * s, _41 * s + _42 * c, _43, _44);
 		}
 
-		Matrix4 & RotationZSelf(float angle)
+		Matrix4 & rotationZSelf(float angle)
 		{
-			return *this = RotationZ(angle);
+			return *this = rotationZ(angle);
 		}
 
-		Matrix4 Translation(const Vector3 & v) const
+		Matrix4 translation(const Vector3 & v) const
 		{
 			return Matrix4(
 				_11 + _14 * v.x, _12 + _14 * v.y, _13 + _14 * v.z, _14,
@@ -861,7 +1163,7 @@ namespace my
 				_41 + _44 * v.x, _42 + _44 * v.y, _43 + _44 * v.z, _44);
 		}
 
-		Matrix4 & TranslationSelf(const Vector3 & v)
+		Matrix4 & translationSelf(const Vector3 & v)
 		{
 			_11 = _11 + _14 * v.x; _12 = _12 + 14 * v.y; _13 = _13 + _14 * v.z;
 			_21 = _21 + _24 * v.x; _22 = _22 + 14 * v.y; _23 = _23 + _24 * v.z;
@@ -873,4 +1175,23 @@ namespace my
 	public:
 		static const Matrix4 identity;
 	};
+
+	inline Vector4 Vector2::transform(const Matrix4 & m) const
+	{
+		return Vector4(x, y, 0, 1).transform(m);
+	}
+
+	inline Vector4 Vector3::transform(const Matrix4 & m) const
+	{
+		return Vector4(x, y, z, 1).transform(m);
+	}
+
+	inline Vector4 Vector4::transform(const Matrix4 & m) const
+	{
+		return Vector4(
+			x * m._11 + y * m._21 + z * m._31 + w * m._41,
+			x * m._12 + y * m._22 + z * m._32 + w * m._42,
+			x * m._13 + y * m._23 + z * m._33 + w * m._43,
+			x * m._14 + y * m._24 + z * m._34 + w * m._44);
+	}
 };
