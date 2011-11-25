31069a3f36bee977fd9dbfaf1f8d9170c151c01b
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2011-11-25 12:19:03 +0000

    add ogreExporter.mll compiled by maya2008
    git-svn-id: svn://127.0.0.1/repos/trunk@80 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- MayaExport/RunMaya.bat
+++ MayaExport/RunMaya.bat
@@ -1,3 +1,3 @@
 set MAYA_PLUG_IN_PATH=%MAYA_PLUG_IN_PATH%;%cd%\..\Release
 set MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;%cd%\mel
-"C:\Program Files\Alias\Maya7.0\bin\maya.exe"
+"C:\Program Files\Autodesk\Maya2008\bin\maya.exe"
