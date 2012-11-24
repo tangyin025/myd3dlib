2b5decaaab9301c2a010d92c91496f128d0f72fc
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2012-11-24 15:14:10 +0000

    CImageView support zoom
    git-svn-id: svn://127.0.0.1/repos/trunk@303 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- ImgRegion/ImgRegionView.h
+++ ImgRegion/ImgRegionView.h
@@ -10,6 +10,53 @@ public:
 	DECLARE_MESSAGE_MAP()
 
 	virtual void OnPrepareDC(CDC* pDC, CPrintInfo* pInfo = NULL);
+
+	void SetScrollSizes(const CSize & sizeTotal);
+
+	void UpdateScrollSize(void);
+
+	void UpdateBars(void);
+
+	BOOL GetTrueClientSize(CSize& size, CSize& sizeSb);
+
+	void GetScrollBarSizes(CSize& sizeSb);
+
+	void GetScrollBarState(CSize sizeClient, CSize& needSb,
+		CSize& sizeRange, CPoint& ptMove, BOOL bInsideClient);
+
+	CPoint GetDeviceScrollPosition() const;
+
+	void ScrollToDevicePosition(POINT ptDev);
+
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+
+	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+
+	virtual BOOL OnScroll(UINT nScrollCode, UINT nPos, BOOL bDoScroll = TRUE);
+
+	virtual BOOL OnScrollBy(CSize sizeScroll, BOOL bDoScroll = TRUE);
+
+	afx_msg BOOL OnMouseWheel(UINT fFlags, short zDelta, CPoint point);
+
+	BOOL DoMouseWheel(UINT fFlags, short zDelta, CPoint point);
+
+	void CheckScrollBars(BOOL& bHasHorzBar, BOOL& bHasVertBar) const;
+
+	int m_ScaleIdx;
+
+	CSize m_totalLog;
+
+	CSize m_totalDev;
+
+	CSize m_pageDev;
+
+	CSize m_lineDev;
+
+	BOOL m_bInsideUpdate;
+
+	BOOL m_bCenter;
 };
 
 class CImgRegionView : public CImageView
