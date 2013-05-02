e0bac1c66054dd0bc2371cd6ed794b2091a4d938
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2013-05-02 06:36:34 +0000

    clear bullet code
    git-svn-id: svn://127.0.0.1/repos/trunk@571 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- demo2_3/GameState.cpp
+++ demo2_3/GameState.cpp
@@ -66,17 +66,6 @@ HRESULT GameStateMain::OnCreateDevice(
 {
 	Game::getSingleton().AddLine(L"GameStateMain::OnCreateDevice", D3DCOLOR_ARGB(255,255,128,0));
 
-	//m_collisionConfiguration.reset(new btDefaultCollisionConfiguration());
-
-	//m_dispatcher.reset(new btCollisionDispatcher(m_collisionConfiguration.get()));
-
-	//m_overlappingPairCache.reset(new btAxisSweep3(btVector3(-1000,-1000,-1000), btVector3(1000,1000,1000)));
-
-	//m_constraintSolver.reset(new btSequentialImpulseConstraintSolver());
-
-	//m_dynamicsWorld.reset(new btDiscreteDynamicsWorld(
-	//	m_dispatcher.get(), m_overlappingPairCache.get(), m_constraintSolver.get(), m_collisionConfiguration.get()));
-
 	m_SimpleSample = Game::getSingleton().LoadEffect("shader/SimpleSample.fx");
 
 	m_ShadowMap = Game::getSingleton().LoadEffect("shader/ShadowMap.fx");
