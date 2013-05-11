155e6f7af27036351efc03f78efcc35dceefc599
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2013-05-11 01:22:26 +0000

    up physx dirs
    git-svn-id: svn://127.0.0.1/repos/trunk@579 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- demo2_3/PhysxSample.cpp
+++ demo2_3/PhysxSample.cpp
@@ -6,7 +6,7 @@
 #pragma comment(lib, "PhysX3Cooking_x86.lib")
 #pragma comment(lib, "PhysX3Extensions.lib")
 #pragma comment(lib, "PhysXProfileSDK.lib")
-#pragma comment(lib, "PxTask.lib")
+//#pragma comment(lib, "PxTask.lib")
 
 void StepperTask::run(void)
 {
