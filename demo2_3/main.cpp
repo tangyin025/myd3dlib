968f74c31bfa03f6748b9ebd0c008186dfbd7fcf
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2015-02-24 07:35:00 +0000

    git-svn-id: svn://127.0.0.1/repos/trunk@903 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- demo2_3/main.cpp
+++ demo2_3/main.cpp
@@ -96,11 +96,24 @@ public:
 		}
 	}
 
-	MeshComponentPtr CreateMeshComponent(my::OgreMeshPtr mesh, bool cloth)
+	MeshComponentPtr CreateMeshComponent(my::OgreMeshPtr mesh, bool cloth, bool bInstance)
 	{
 		MeshComponentPtr mesh_cmp(new MeshComponent(mesh->m_aabb));
-		MeshComponent::LODPtr lod(cloth ? new ClothMeshComponentLOD(mesh_cmp.get()) : new MeshComponent::LOD(mesh_cmp.get()));
+		MeshComponent::LODPtr lod;
+		if (cloth)
+		{
+			lod.reset(new ClothMeshComponentLOD(mesh_cmp.get()));
+		}
+		else
+		{
+			lod.reset(new MeshComponent::LOD(mesh_cmp.get()));
+		}
 		lod->m_Mesh = mesh;
+		if (bInstance)
+		{
+			lod->m_Mesh->CreateInstance(Game::GetD3D9Device());
+			lod->m_bInstance = true;
+		}
 		std::vector<std::string>::const_iterator mat_name_iter = lod->m_Mesh->m_MaterialNameList.begin();
 		for(; mat_name_iter != lod->m_Mesh->m_MaterialNameList.end(); mat_name_iter++)
 		{
@@ -147,9 +160,7 @@ public:
 
 		//m_mesh_ins = LoadMesh("mesh/tube.mesh.xml");
 		//m_mesh_ins->CreateInstance(pd3dDevice);
-		m_mesh_ins = CreateMeshComponent(LoadMesh("mesh/tube.mesh.xml"), false);
-		m_mesh_ins->m_lods[0]->m_Mesh->CreateInstance(pd3dDevice);
-		m_mesh_ins->m_lods[0]->m_bInstance = true;
+		m_mesh_ins = CreateMeshComponent(LoadMesh("mesh/tube.mesh.xml"), false, true);
 
 		//// ========================================================================================================
 		//// ´ó³¡¾°
@@ -183,7 +194,7 @@ public:
 
 		m_cloth_anim = LoadSkeleton("mesh/cloth.skeleton.xml");
 
-		m_cloth_mesh = CreateMeshComponent(LoadMesh("mesh/cloth.mesh.xml"), true);
+		m_cloth_mesh = CreateMeshComponent(LoadMesh("mesh/cloth.mesh.xml"), true, false);
 		dynamic_pointer_cast<ClothMeshComponentLOD>(m_cloth_mesh->m_lods[0])->CreateCloth(
 			this, m_cloth_anim->m_boneHierarchy, m_cloth_anim->GetBoneIndex("joint5"), PxClothCollisionData());
 		//m_cloth_mesh_vertices.reset(new Cache(
