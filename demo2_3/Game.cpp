699f979f3b51ccdf2b4c222753b0f540db052c29
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2015-10-01 04:10:47 +0000

    fix Game::m_SkyLightCam
    git-svn-id: svn://127.0.0.1/repos/trunk@1046 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- demo2_3/Game.cpp
+++ demo2_3/Game.cpp
@@ -88,7 +88,7 @@ Game::Game(void)
 		m_DownFilterRT[i].reset(new Texture2D());
 	}
 	m_Camera.reset(new FirstPersonCamera(D3DXToRadian(75.0f),1.333333f,0.1f,3000.0f));
-	m_SkyLightCam.reset(new my::OrthoCamera(30,30,-100,100));
+	m_SkyLightCam.reset(new my::OrthoCamera(sqrt(30*30*2.0f),1.0f,-100,100));
 }
 
 Game::~Game(void)
