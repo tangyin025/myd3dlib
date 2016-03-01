e61ca7a89805562e7c74d9a2cd7be38f56c7d250
tang <tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2016-03-01 14:19:34 +0000

    Terrain::FillVert, Terrain::EdgeNv1, Terrain::EdgeNvM, Terrain::FillNvM, Terrain::GetFragment
    git-svn-id: svn://127.0.0.1/repos/trunk@1191 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- myd3dbox/Component/Terrain.cpp
+++ myd3dbox/Component/Terrain.cpp
@@ -24,6 +24,16 @@ TerrainChunk::TerrainChunk(Terrain * Owner, const my::Vector2 & PosStart, const 
 {
 }
 
+TerrainChunk::TerrainChunk(void)
+	: m_Owner(NULL)
+	, m_aabb(my::AABB(0,1))
+	, m_PosStart(0,0)
+	, m_PosEnd(1,1)
+	, m_TexStart(0,0)
+	, m_TexEnd(1,1)
+{
+}
+
 TerrainChunk::~TerrainChunk(void)
 {
 	_ASSERT(!m_vb.m_ptr);
@@ -91,53 +101,65 @@ void TerrainChunk::DestroyVertices(void)
 	m_vb.OnDestroyDevice();
 }
 
-template <int N>
-unsigned int FillVert(Terrain::VertTable2D & verts, int hs, unsigned int k)
+template <class T, int N>
+unsigned int FillVert(T & tri, int hs, unsigned int k)
 {
     _ASSERT((hs & (hs - 1)) == 0);
     for (int i = 0; i < N - 1; i += hs * 2)
     {
         for (int j = 0; j < N - 1; j += hs * 2)
         {
-            verts[i + hs * 0][j + hs * 1] = k++;
-            verts[i + hs * 1][j + hs * 0] = k++;
-            verts[i + hs * 1][j + hs * 1] = k++;
+            tri.set(i + hs * 0, j + hs * 1, k++);
+            tri.set(i + hs * 1, j + hs * 0, k++);
+            tri.set(i + hs * 1, j + hs * 1, k++);
             if (j + hs * 2 >= N - 1)
             {
-                verts[i + hs * 1][j + hs * 2] = k++;
+                tri.set(i + hs * 1, j + hs * 2, k++);
             }
             if (i + hs * 2 >= N - 1)
             {
-                verts[i + hs * 2][j + hs * 1] = k++;
+                tri.set(i + hs * 2, j + hs * 1, k++);
             }
         }
     }
 
     if (hs / 2 > 0)
     {
-        k = FillVert<N>(verts, hs / 2, k);
+        k = FillVert<T, N>(tri, hs / 2, k);
     }
     return k;
 }
 
-template <int N>
-unsigned int FillVert(Terrain::VertTable2D & verts)
+template <class T, int N>
+unsigned int FillVert(T & tri)
 {
     BOOST_STATIC_ASSERT(((N - 1) & (N - 2)) == 0);
     unsigned int k = 0;
-    verts[0][0] = k++;
-    verts[0][N - 1] = k++;
-    verts[N - 1][0] = k++;
-    verts[N - 1][N - 1] = k++;
-    return FillVert<N>(verts, (N - 1) / 2, k);
+    tri.set(0, 0, k++);
+    tri.set(0, N - 1, k++);
+    tri.set(N - 1, 0, k++);
+    tri.set(N - 1, N - 1, k++);
+    return FillVert<T, N>(tri, (N - 1) / 2, k);
 }
 
-Terrain::VertTable2D::VertTable2D(void)
+Terrain::VertexArray2D::VertexArray2D(void)
 {
-	FillVert<static_size>(*this);
+    struct Tri
+    {
+        VertexArray2D & verts;
+		Tri(VertexArray2D & _verts)
+			: verts(_verts)
+		{
+		}
+        void set(int i, int j, int k)
+        {
+			verts[i][j] = k;
+        }
+    };
+	FillVert<Tri, static_size>(Tri(*this));
 }
 
-const Terrain::VertTable2D Terrain::m_VertTable;
+const Terrain::VertexArray2D Terrain::m_VertTable;
 
 Terrain::Terrain(const my::Matrix4 & World, float HeightScale, float RowScale, float ColScale, float WrappedU, float WrappedV)
 	: RenderComponent(my::AABB(Vector3(0,-1,0), Vector3(m_RowChunks * m_ChunkRows * RowScale, 1, m_RowChunks * m_ChunkRows * ColScale)), ComponentTypeTerrain)
@@ -150,8 +172,18 @@ Terrain::Terrain(const my::Matrix4 & World, float HeightScale, float RowScale, f
 	, m_Root(Vector3(0,-1000,0), Vector3(m_RowChunks * m_ChunkRows * RowScale, 1000, m_RowChunks * m_ChunkRows * ColScale), 1.0f)
 {
 	memset(&m_Samples, 0, sizeof(m_Samples));
+	for (unsigned int i = 0; i < ChunkArray::static_size; i++)
+	{
+		for (unsigned int j = 0; j < ChunkArray::static_size; j++)
+		{
+			m_Chunks[i][j].reset(new TerrainChunk(this,
+				my::Vector2((i + 0) * m_ChunkRows * m_RowScale, (j + 0) * m_ChunkRows * m_ColScale),
+				my::Vector2((i + 1) * m_ChunkRows * m_RowScale, (j + 1) * m_ChunkRows * m_ColScale),
+				my::Vector2(0,0), my::Vector2(m_WrappedU,m_WrappedV)));
+			m_Root.AddComponent(m_Chunks[i][j].get(), m_Chunks[i][j]->m_aabb, 0.1f);
+		}
+	}
 	CreateElements();
-	CreateChunks();
 	CreateRigidActor(m_World);
 	CreateShape();
 }
@@ -174,19 +206,18 @@ Terrain::~Terrain(void)
 		ReleaseResource();
 	}
 	_ASSERT(!m_Decl);
-	_ASSERT(!m_ib.m_ptr);
 	m_Root.ClearAllComponents();
 }
 
 void Terrain::UpdateSamples(my::Texture2DPtr HeightMap)
 {
 	D3DSURFACE_DESC desc = HeightMap->GetLevelDesc(0);
-	if (desc.Format == D3DFMT_L8 && desc.Width >= Sample::static_size && desc.Height >= Sample::static_size)
+	if (desc.Format == D3DFMT_L8 && desc.Width >= SampleArray::static_size && desc.Height >= SampleArray::static_size)
 	{
 		D3DLOCKED_RECT lrc = HeightMap->LockRect(NULL, D3DLOCK_READONLY, 0);
-		for (unsigned int i = 0; i < Sample::static_size; i++)
+		for (unsigned int i = 0; i < SampleArray::static_size; i++)
 		{
-			for (unsigned int j = 0; j < Sample::static_size; j++)
+			for (unsigned int j = 0; j < SampleArray::static_size; j++)
 			{
 				m_Samples[i][j] = *((unsigned char *)lrc.pBits + j * lrc.Pitch + i);
 			}
@@ -199,6 +230,7 @@ void Terrain::UpdateSamples(my::Texture2DPtr HeightMap)
 
 void Terrain::UpdateChunks(void)
 {
+	m_aabb = AABB(-1,1);
 	for (unsigned int i = 0; i < ChunkArray::static_size; i++)
 	{
 		for (unsigned int j = 0; j < ChunkArray::static_size; j++)
@@ -213,25 +245,9 @@ void Terrain::UpdateChunks(void)
 
 float Terrain::GetSampleHeight(float x, float z)
 {
-	int i = Clamp<int>((int)(x / m_RowScale), 0, Sample::static_size - 1);
-	int j = Clamp<int>((int)(z / m_ColScale), 0, Sample::static_size - 1);
+	int i = Clamp<int>((int)(x / m_RowScale), 0, SampleArray::static_size - 1);
+	int j = Clamp<int>((int)(z / m_ColScale), 0, SampleArray::static_size - 1);
 	return m_Samples[i][j] * m_HeightScale;
-}
-
-void Terrain::CreateChunks(void)
-{
-	for (unsigned int i = 0; i < ChunkArray::static_size; i++)
-	{
-		for (unsigned int j = 0; j < ChunkArray::static_size; j++)
-		{
-			m_Chunks[i][j].reset(new TerrainChunk(this,
-				my::Vector2((i + 0) * m_ChunkRows * m_RowScale, (j + 0) * m_ChunkRows * m_ColScale),
-				my::Vector2((i + 1) * m_ChunkRows * m_RowScale, (j + 1) * m_ChunkRows * m_ColScale),
-				my::Vector2(0,0), my::Vector2(m_WrappedU,m_WrappedV)));
-			m_Root.AddComponent(m_Chunks[i][j].get(), m_Chunks[i][j]->m_aabb, 0.1f);
-			m_aabb.unionSelf(m_Chunks[i][j]->m_aabb);
-		}
-	}
 }
 
 void Terrain::CreateRigidActor(const my::Matrix4 & World)
@@ -243,21 +259,21 @@ void Terrain::CreateRigidActor(const my::Matrix4 & World)
 
 void Terrain::CreateHeightField(void)
 {
-	std::vector<PxHeightFieldSample> Samples(Sample::static_size * Sample::static_size);
-	for (unsigned int i = 0; i < Sample::static_size; i++)
+	std::vector<PxHeightFieldSample> Samples(SampleArray::static_size * SampleArray::static_size);
+	for (unsigned int i = 0; i < SampleArray::static_size; i++)
 	{
-		for (unsigned int j = 0; j < Sample::static_size; j++)
+		for (unsigned int j = 0; j < SampleArray::static_size; j++)
 		{
-			Samples[i * Sample::static_size + j].height = m_Samples[i][j];
-			Samples[i * Sample::static_size + j].materialIndex0 = PxBitAndByte(0, false);
-			Samples[i * Sample::static_size + j].materialIndex1 = PxBitAndByte(0, false);
+			Samples[i * SampleArray::static_size + j].height = m_Samples[i][j];
+			Samples[i * SampleArray::static_size + j].materialIndex0 = PxBitAndByte(0, false);
+			Samples[i * SampleArray::static_size + j].materialIndex1 = PxBitAndByte(0, false);
 		}
 	}
 
 	PxHeightFieldDesc hfDesc;
 	hfDesc.format             = PxHeightFieldFormat::eS16_TM;
-	hfDesc.nbColumns          = Sample::static_size;
-	hfDesc.nbRows             = Sample::static_size;
+	hfDesc.nbColumns          = SampleArray::static_size;
+	hfDesc.nbRows             = SampleArray::static_size;
 	hfDesc.samples.data       = &Samples[0];
 	hfDesc.samples.stride     = sizeof(Samples[0]);
 	m_HeightField.reset(PhysXContext::getSingleton().m_sdk->createHeightField(hfDesc));
@@ -297,6 +313,180 @@ void Terrain::CreateElements(void)
 	m_VertexStride = offset;
 }
 
+template <class T>
+unsigned int EdgeNv1(T & tri, int N, int r0, int rs, int c0, int cs)
+{
+    _ASSERT((N & (N - 1)) == 0);
+    unsigned int k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int cb = c0 + cs * i;
+		if (i < N - 1)
+		{
+			tri.set(k + 0, r0, cb);
+			tri.set(k + 1, r0, cb + cs);
+			tri.set(k + 2, r0 + rs, cb + cs);
+			k += 3;
+		}
+		else
+		{
+			tri.set(k + 0, r0, cb);
+			tri.set(k + 1, r0, cb + cs);
+			tri.set(k + 2, r0 + rs, cb);
+			k += 3;
+		}
+	}
+	return k;
+};
+
+template <class T>
+unsigned int EdgeNvM(T & tri, int N, int M, int r0, int rs, int c0, int cs)
+{
+    _ASSERT((N & (N - 1)) == 0);
+    _ASSERT((M & (M - 1)) == 0);
+	if (M == 1)
+	{
+		return EdgeNv1(tri, N, r0, rs, c0, cs);
+	}
+    unsigned int k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int cb = c0 + cs * i * M;
+
+        tri.set(k + 0, r0, cb);
+        tri.set(k + 1, r0, cb + cs * M);
+        tri.set(k + 2, r0 + rs, cb + cs * M / 2);
+        k += 3;
+
+        int j = 0;
+        for (; j < M / 2; j++)
+        {
+            if (i > 0 || j > 0)
+            {
+                tri.set(k + 0, r0 + rs, cb + cs * j);
+                tri.set(k + 1, r0, cb);
+                tri.set(k + 2, r0 + rs, cb + cs * (j + 1));
+                k += 3;
+            }
+        }
+
+        for (; j < M; j++)
+        {
+            if (i < N - 1 || j < M - 1)
+            {
+                tri.set(k + 0, r0 + rs, cb + cs * j);
+                tri.set(k + 1, r0, cb + cs * M);
+                tri.set(k + 2, r0 + rs, cb + cs * (j + 1));
+                k += 3;
+            }
+        }
+    }
+    return k;
+}
+
+template <class T>
+unsigned int FillNvM(T & tri, int N, int M, int r0, int rs, int c0, int cs)
+{
+    unsigned int k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            tri.set(k + 0, r0 + rs * (i + 0), c0 + cs * (j + 0));
+            tri.set(k + 1, r0 + rs * (i + 0), c0 + cs * (j + 1));
+            tri.set(k + 2, r0 + rs * (i + 1), c0 + cs * (j + 0));
+            k += 3;
+
+            tri.set(k + 0, r0 + rs * (i + 0), c0 + cs * (j + 1));
+            tri.set(k + 1, r0 + rs * (i + 1), c0 + cs * (j + 1));
+            tri.set(k + 2, r0 + rs * (i + 1), c0 + cs * (j + 0));
+            k += 3;
+        }
+    }
+    return k;
+}
+
+const Terrain::Fragment & Terrain::GetFragment(unsigned char center, unsigned char left, unsigned char top, unsigned char right, unsigned char bottom)
+{
+	unsigned int id = ((center & 7) << 12) | ((left & 7) << 9) | ((top & 7) << 6) | ((right & 7) << 3) | ((bottom & 7) << 0);
+	FragmentMap::iterator frag_iter = m_Fragment.find(id);
+	if (frag_iter != m_Fragment.end())
+	{
+		return frag_iter->second;
+	}
+
+    struct Tri
+    {
+        WORD * buff;
+		Tri(WORD * _buff)
+			: buff(_buff)
+		{
+		}
+        void set(int k, int i, int j)
+        {
+			buff[k] = m_VertTable[i][j];
+        }
+    };
+
+    struct TriTranspose
+    {
+        WORD * buff;
+		TriTranspose(WORD * _buff)
+			: buff(_buff)
+		{
+		}
+        void set(int k, int i, int j)
+        {
+			buff[k] = m_VertTable[j][i];
+        }
+    };
+
+	IDirect3DDevice9 * pd3dDevice = D3DContext::getSingleton().m_d3dDevice;
+	Fragment & frag = m_Fragment[id];
+	if (left > center || top > center || right > center || bottom > center)
+	{
+		const int N[] = {
+			m_ChunkRows >> center,
+			m_ChunkRows >> Max(center, left),
+			m_ChunkRows >> Max(center, top),
+			m_ChunkRows >> Max(center, right),
+			m_ChunkRows >> Max(center, bottom)
+		};
+		const int M[] = {
+			N[0] / N[0],
+			N[0] / N[1],
+			N[0] / N[2],
+			N[0] / N[3],
+			N[0] / N[4]
+		};
+		frag.VertNum = (N[0] + 1) * (N[0] + 1);
+		frag.PrimitiveCount = (N[0] - 2) * (N[0] - 2) * 2
+			+ N[1] * (M[1] + 1) + N[2] * (M[2] + 1) + N[3] * (M[3] + 1) + N[4] * (M[4] + 1) - 8;
+		frag.ib.CreateIndexBuffer(pd3dDevice, frag.PrimitiveCount * 3 * sizeof(WORD), 0, D3DFMT_INDEX16, D3DPOOL_MANAGED);
+		VOID * pIndices = frag.ib.Lock(0, 0, 0);
+		unsigned int k = 0;
+		const int step = 1 << center;
+		k += FillNvM(Tri((WORD *)pIndices + k), N[0] - 2, N[0] - 2, 0 + step, step, 0 + step, step);
+		k += EdgeNvM(TriTranspose((WORD *)pIndices + k), N[1], M[1], 0, step, N[0] * step, -step);
+		k += EdgeNvM(Tri((WORD *)pIndices + k), N[2], M[2], 0, step, 0, step);
+		k += EdgeNvM(TriTranspose((WORD *)pIndices + k), N[3], M[3], N[0] * step, -step, 0, step);
+		k += EdgeNvM(Tri((WORD *)pIndices + k), N[4], M[4], N[0] * step, -step, N[0] * step, -step);
+		frag.ib.Unlock();
+	}
+	else
+	{
+		const int N = m_ChunkRows >> center;
+		frag.VertNum = (N + 1) * (N + 1);
+		frag.PrimitiveCount = N * N * 2;
+		frag.ib.CreateIndexBuffer(pd3dDevice, frag.PrimitiveCount * 3 * sizeof(WORD), 0, D3DFMT_INDEX16, D3DPOOL_MANAGED);
+		VOID * pIndices = frag.ib.Lock(0, 0, 0);
+		const int step = 1 << center;
+		unsigned int k = FillNvM(Tri((WORD *)pIndices), N, N, 0, step, 0, step);
+		frag.ib.Unlock();
+	}
+	return frag;
+}
+
 template<>
 void Terrain::save<boost::archive::xml_oarchive>(boost::archive::xml_oarchive & ar, const unsigned int version) const
 {
@@ -309,6 +499,7 @@ void Terrain::save<boost::archive::xml_oarchive>(boost::archive::xml_oarchive & 
 	ar << BOOST_SERIALIZATION_NVP(m_WrappedV);
 	ar << BOOST_SERIALIZATION_NVP(m_Material);
 	ar << boost::serialization::make_nvp("Samples", boost::serialization::binary_object((void *)&m_Samples, sizeof(m_Samples)));
+	ar << BOOST_SERIALIZATION_NVP(m_Chunks);
 }
 
 template<>
@@ -323,8 +514,16 @@ void Terrain::load<boost::archive::xml_iarchive>(boost::archive::xml_iarchive & 
 	ar >> BOOST_SERIALIZATION_NVP(m_WrappedV);
 	ar >> BOOST_SERIALIZATION_NVP(m_Material);
 	ar >> boost::serialization::make_nvp("Samples", boost::serialization::binary_object((void *)&m_Samples, sizeof(m_Samples)));
+	ar >> BOOST_SERIALIZATION_NVP(m_Chunks);
+	for (unsigned int i = 0; i < ChunkArray::static_size; i++)
+	{
+		for (unsigned int j = 0; j < ChunkArray::static_size; j++)
+		{
+			m_Chunks[i][j]->m_Owner = this;
+			m_Root.AddComponent(m_Chunks[i][j].get(), m_Chunks[i][j]->m_aabb, 0.1f);
+		}
+	}
 	CreateElements();
-	CreateChunks();
 	CreateRigidActor(m_World);
 	CreateShape();
 }
@@ -340,27 +539,6 @@ void Terrain::RequestResource(void)
 	elems.push_back(ve_end);
 	HRESULT hr;
 	V(pd3dDevice->CreateVertexDeclaration(&elems[0], &m_Decl));
-
-	m_ib.CreateIndexBuffer(pd3dDevice, sizeof(WORD) * m_ChunkRows * m_ChunkRows * 6, 0, D3DFMT_INDEX16, D3DPOOL_MANAGED);
-	VOID * pIndices = m_ib.Lock(0, 0, 0);
-	if (pIndices)
-	{
-		for (unsigned int i = 0; i < m_ChunkRows; i++)
-		{
-			for (unsigned int j = 0; j < m_ChunkRows; j++)
-			{
-				int index = (i * m_ChunkRows + j) * 6;
-				*((WORD *)pIndices + index + 0) = (WORD)m_VertTable[i + 0][j + 0];
-				*((WORD *)pIndices + index + 1) = (WORD)m_VertTable[i + 0][j + 1];
-				*((WORD *)pIndices + index + 2) = (WORD)m_VertTable[i + 1][j + 0];
-
-				*((WORD *)pIndices + index + 3) = (WORD)m_VertTable[i + 0][j + 1];
-				*((WORD *)pIndices + index + 4) = (WORD)m_VertTable[i + 1][j + 1];
-				*((WORD *)pIndices + index + 5) = (WORD)m_VertTable[i + 1][j + 0];
-			}
-		}
-		m_ib.Unlock();
-	}
 
 	m_Material->RequestResource();
 
@@ -378,7 +556,7 @@ void Terrain::RequestResource(void)
 void Terrain::ReleaseResource(void)
 {
 	m_Decl.Release();
-	m_ib.OnDestroyDevice();
+	m_Fragment.clear();
 	m_Material->ReleaseResource();
 	for (unsigned int i = 0; i < ChunkArray::static_size; i++)
 	{
@@ -417,16 +595,11 @@ void Terrain::AddToPipeline(const my::Frustum & frustum, RenderPipeline * pipeli
 		}
 		void operator() (my::OctComponent * oct_cmp, my::IntersectionTests::IntersectionType)
 		{
+			unsigned char center=2, left=3, top=4, right=5, bottom=0;
+			const Fragment & frag = terrain->GetFragment(center, left, top, right, bottom);
 			TerrainChunk * chunk = dynamic_cast<TerrainChunk *>(oct_cmp);
-			pipeline->PushIndexedPrimitive(
-				PassID,
-				terrain->m_Decl,
-				chunk->m_vb.m_ptr,
-				terrain->m_ib.m_ptr,
-				D3DPT_TRIANGLELIST, 0, 0,
-				(terrain->m_ChunkRows + 1) * (terrain->m_ChunkRows + 1),
-				terrain->m_VertexStride, 0,
-				terrain->m_ChunkRows * terrain->m_ChunkRows * 2, 0, shader, terrain);
+			pipeline->PushIndexedPrimitive(PassID, terrain->m_Decl, chunk->m_vb.m_ptr,
+				frag.ib.m_ptr, D3DPT_TRIANGLELIST, 0, 0, frag.VertNum, terrain->m_VertexStride, 0, frag.PrimitiveCount, 0, shader, terrain);
 		}
 	};
 
--- myd3dbox/Component/Terrain.h
+++ myd3dbox/Component/Terrain.h
@@ -10,17 +10,35 @@ class TerrainChunk
 {
 public:
 	Terrain * m_Owner;
+
 	my::AABB m_aabb;
-	my::VertexBuffer m_vb;
+
 	my::Vector2 m_PosStart;
+
 	my::Vector2 m_PosEnd;
+
 	my::Vector2 m_TexStart;
+
 	my::Vector2 m_TexEnd;
+
+	my::VertexBuffer m_vb;
 
 public:
 	TerrainChunk(Terrain * Owner, const my::Vector2 & PosStart, const my::Vector2 & PosEnd, const my::Vector2 & TexStart, const my::Vector2 & TexEnd);
 
+	TerrainChunk(void);
+
 	virtual ~TerrainChunk(void);
+
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version)
+	{
+		ar & BOOST_SERIALIZATION_NVP(m_aabb);
+		ar & BOOST_SERIALIZATION_NVP(m_PosStart);
+		ar & BOOST_SERIALIZATION_NVP(m_PosEnd);
+		ar & BOOST_SERIALIZATION_NVP(m_TexStart);
+		ar & BOOST_SERIALIZATION_NVP(m_TexEnd);
+	}
 
 	void CreateVertices(void);
 
@@ -35,35 +53,68 @@ class Terrain
 	: public RenderComponent
 {
 public:
-	static const DWORD m_RowChunks = 8;
+	static const DWORD m_RowChunks = 1;
+
 	static const DWORD m_ChunkRows = 64;
-	typedef boost::array<unsigned short, m_ChunkRows + 1> VertTable;
-	class VertTable2D
-		: public boost::array<VertTable, VertTable::static_size>
+
+	typedef boost::array<unsigned short, m_ChunkRows + 1> VertexArray;
+
+	class VertexArray2D
+		: public boost::array<VertexArray, VertexArray::static_size>
 	{
 	public:
-		VertTable2D(void);
+		VertexArray2D(void);
 	};
-	static const VertTable2D m_VertTable;
+
+	static const VertexArray2D m_VertTable;
+
 	my::Matrix4 m_World;
+
 	float m_HeightScale;
+
 	float m_RowScale;
+
 	float m_ColScale;
+
 	float m_WrappedU;
+
 	float m_WrappedV;
-	typedef boost::array<unsigned char, m_RowChunks * m_ChunkRows + 1> Sample;
-	typedef boost::array<Sample, Sample::static_size> Sample2D;
-	Sample2D m_Samples;
+
+	typedef boost::array<unsigned char, m_RowChunks * m_ChunkRows + 1> SampleArray;
+
+	typedef boost::array<SampleArray, SampleArray::static_size> SampleArray2D;
+
+	SampleArray2D m_Samples;
+
 	my::D3DVertexElementSet m_VertexElems;
+
 	DWORD m_VertexStride;
+
 	CComPtr<IDirect3DVertexDeclaration9> m_Decl;
-	my::IndexBuffer m_ib;
+
+	struct Fragment
+	{
+		unsigned int VertNum;
+		unsigned int PrimitiveCount;
+		my::IndexBuffer ib;
+	};
+
+	typedef boost::unordered_map<unsigned int, Fragment> FragmentMap;
+
+	FragmentMap m_Fragment;
+
 	MaterialPtr m_Material;
+
 	my::OctRoot m_Root;
+
 	typedef boost::array<TerrainChunkPtr, m_RowChunks> ChunkArray;
+
 	typedef boost::array<ChunkArray, ChunkArray::static_size> ChunkArray2D;
+
 	ChunkArray2D m_Chunks;
+
 	PhysXPtr<PxHeightField> m_HeightField;
+
 	PhysXPtr<PxRigidActor> m_RigidActor;
 
 	void UpdateSamples(my::Texture2DPtr HeightMap);
@@ -71,8 +122,6 @@ public:
 	void UpdateChunks(void);
 
 	float GetSampleHeight(float x, float z);
-
-	void CreateChunks(void);
 
 	void CreateRigidActor(const my::Matrix4 & World);
 
@@ -83,6 +132,8 @@ public:
 	void UpdateShape(void);
 
 	void CreateElements(void);
+
+	const Fragment & GetFragment(unsigned char center, unsigned char left, unsigned char top, unsigned char right, unsigned char bottom);
 
 public:
 	Terrain(const my::Matrix4 & World, float HeightScale, float RowScale, float ColScale, float WrappedU, float WrappedV);
