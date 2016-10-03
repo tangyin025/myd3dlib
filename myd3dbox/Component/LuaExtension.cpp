7a6ae37c8dd777be70c0b8bb92bf4c142985f6ad
Tang <Tang@a5c22d9f-9f17-4845-9224-d5bed68bb274>
2016-10-03 14:19:24 +0000

    lua extension coding
    git-svn-id: svn://127.0.0.1/repos/trunk@1257 a5c22d9f-9f17-4845-9224-d5bed68bb274

--- myd3dbox/Component/LuaExtension.cpp
+++ myd3dbox/Component/LuaExtension.cpp
@@ -12,11 +12,21 @@ namespace luabind
 	struct default_converter<std::wstring>
 		: native_converter_base<std::wstring>
 	{
-		static int compute_score(lua_State* L, int index);
+		static int compute_score(lua_State* L, int index)
+		{
+			return lua_type(L, index) == LUA_TSTRING ? 0 : -1;
+		}
 
-		std::wstring from(lua_State* L, int index);
+		std::wstring from(lua_State* L, int index)
+		{
+			return u8tows(lua_tostring(L, index));
+		}
 
-		void to(lua_State* L, std::wstring const& value);
+		void to(lua_State* L, std::wstring const& value)
+		{
+			std::string str = wstou8(value);
+			lua_pushlstring(L, str.data(), str.size());
+		}
 	};
 
 	template <>
@@ -35,11 +45,32 @@ namespace luabind
 	struct default_converter<my::ControlEvent>
 		: native_converter_base<my::ControlEvent>
 	{
-		static int compute_score(lua_State * L, int index);
+		static int compute_score(lua_State * L, int index)
+		{
+			return lua_type(L, index) == LUA_TFUNCTION ? 0 : -1;
+		}
 
-		my::ControlEvent from(lua_State * L, int index);
+		my::ControlEvent from(lua_State * L, int index)
+		{
+			struct InternalExceptionHandler
+			{
+				luabind::object obj;
+				InternalExceptionHandler(const luabind::object & _obj)
+					: obj(_obj)
+				{
+				}
+				void operator()(my::EventArgs * args)
+				{
+					obj(args);
+				}
+			};
+			return InternalExceptionHandler(luabind::object(luabind::from_stack(L, index)));
+		}
 
-		void to(lua_State * L, my::ControlEvent const & e);
+		void to(lua_State * L, my::ControlEvent const & e)
+		{
+			_ASSERT(false);
+		}
 	};
 
 	template <>
@@ -52,11 +83,32 @@ namespace luabind
 	struct default_converter<my::TimerEvent>
 		: native_converter_base<my::TimerEvent>
 	{
-		static int compute_score(lua_State * L, int index);
+		static int compute_score(lua_State * L, int index)
+		{
+			return lua_type(L, index) == LUA_TFUNCTION ? 0 : -1;
+		}
 
-		my::TimerEvent from(lua_State * L, int index);
+		my::TimerEvent from(lua_State * L, int index)
+		{
+			struct InternalExceptionHandler
+			{
+				luabind::object obj;
+				InternalExceptionHandler(const luabind::object & _obj)
+					: obj(_obj)
+				{
+				}
+				void operator()(float interval)
+				{
+					obj(interval);
+				}
+			};
+			return InternalExceptionHandler(luabind::object(luabind::from_stack(L, index)));
+		}
 
-		void to(lua_State * L, my::TimerEvent const & e);
+		void to(lua_State * L, my::TimerEvent const & e)
+		{
+			_ASSERT(false);
+		}
 	};
 
 	template <>
@@ -64,92 +116,6 @@ namespace luabind
 		: default_converter<my::TimerEvent>
 	{
 	};
-
-	int default_converter<std::wstring>::compute_score(lua_State* L, int index)
-	{
-		return lua_type(L, index) == LUA_TSTRING ? 0 : -1;
-	}
-
-	std::wstring default_converter<std::wstring>::from(lua_State* L, int index)
-	{
-		return u8tows(lua_tostring(L, index));
-	}
-
-	void default_converter<std::wstring>::to(lua_State* L, std::wstring const& value)
-	{
-		std::string str = wstou8(value);
-		lua_pushlstring(L, str.data(), str.size());
-	}
-
-	int default_converter<my::ControlEvent>::compute_score(lua_State * L, int index)
-	{
-		return lua_type(L, index) == LUA_TFUNCTION ? 0 : -1;
-	}
-
-	my::ControlEvent default_converter<my::ControlEvent>::from(lua_State * L, int index)
-	{
-		struct InternalExceptionHandler
-		{
-			luabind::object obj;
-			InternalExceptionHandler(const luabind::object & _obj)
-				: obj(_obj)
-			{
-			}
-			void operator()(my::EventArgs * args)
-			{
-				//try
-				//{
-					obj(args);
-				//}
-				//catch(const luabind::error & e)
-				//{
-				//	// ! ControlEvent事件处理是容错的，当事件处理失败后，程序继续运行
-				//	Game::getSingleton().AddLine(ms2ws(lua_tostring(e.state(), -1)));
-				//}
-			}
-		};
-		return InternalExceptionHandler(luabind::object(luabind::from_stack(L, index)));
-	}
-
-	void default_converter<my::ControlEvent>::to(lua_State * L, my::ControlEvent const & e)
-	{
-		_ASSERT(false);
-	}
-
-	int default_converter<my::TimerEvent>::compute_score(lua_State * L, int index)
-	{
-		return lua_type(L, index) == LUA_TFUNCTION ? 0 : -1;
-	}
-
-	my::TimerEvent default_converter<my::TimerEvent>::from(lua_State * L, int index)
-	{
-		struct InternalExceptionHandler
-		{
-			luabind::object obj;
-			InternalExceptionHandler(const luabind::object & _obj)
-				: obj(_obj)
-			{
-			}
-			void operator()(float interval)
-			{
-				//try
-				//{
-					obj(interval);
-				//}
-				//catch(const luabind::error & e)
-				//{
-				//	// ! TimerEvent事件处理是容错的，当事件处理失败后，程序继续运行
-				//	Game::getSingleton().AddLine(ms2ws(lua_tostring(e.state(), -1)));
-				//}
-			}
-		};
-		return InternalExceptionHandler(luabind::object(luabind::from_stack(L, index)));
-	}
-
-	void default_converter<my::TimerEvent>::to(lua_State * L, my::TimerEvent const & e)
-	{
-		_ASSERT(false);
-	}
 }
 
 static int add_file_and_line(lua_State * L)
