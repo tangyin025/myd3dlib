992b2e87229c19d783389b72c45d1965730a8e6d
tangyin025 <tangyin025@126.com>
2017-05-16 15:56:17 +0800

    WorldL::ResetViewedActors

--- myd3dbox/Component/World.cpp
+++ myd3dbox/Component/World.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
 #include "World.h"
 #include "Actor.h"
+#include "PhysXContext.h"
 
 using namespace my;
 
@@ -31,7 +32,7 @@ void WorldL::_QueryRenderComponent(const CPoint & level_id, const CPoint & offse
 	if (new_level_id.x >= 0 && new_level_id.x < m_Dim && new_level_id.y >= 0 && new_level_id.y < m_Dim)
 	{
 		Frustum loc_frustum = frustum.transform(Matrix4::Translation(offset.x * 512.0f, 0, offset.y * 512.0f).transpose());
-		m_levels[new_level_id.y * m_Dim + new_level_id.x].QueryActor(frustum, &CallBack(frustum, pipeline, PassMask));
+		m_levels[new_level_id.y * m_Dim + new_level_id.x].QueryActor(loc_frustum, &CallBack(loc_frustum, pipeline, PassMask));
 	}
 }
 
@@ -52,8 +53,76 @@ void WorldL::QueryRenderComponent(const CPoint & level_id, const my::Frustum & f
 
 void WorldL::_ResetViewedActors(const CPoint & level_id, const CPoint & offset, const my::Vector3 & ViewedPos, const my::Vector3 & TargetPos)
 {
+	struct CallBack : public my::IQueryCallback
+	{
+		WorldL * world;
+		const my::Vector3 & ViewedPos;
+		const my::Vector3 & TargetPos;
+		CallBack(WorldL * _world, const my::Vector3 & _ViewedPos, const my::Vector3 & _TargetPos)
+			: world(_world)
+			, ViewedPos(_ViewedPos)
+			, TargetPos(_TargetPos)
+		{
+		}
+		void operator() (OctActor * oct_actor, IntersectionTests::IntersectionType)
+		{
+			_ASSERT(dynamic_cast<Actor *>(oct_actor));
+			Actor * actor = static_cast<Actor *>(oct_actor);
+			OctActorSet::iterator actor_iter = world->m_ViewedActors.find(actor);
+			if (actor_iter == world->m_ViewedActors.end())
+			{
+				if (!actor->IsRequested())
+				{
+					actor->RequestResource();
+				}
+				world->m_ViewedActors.insert(actor);
+				actor->OnEnterPxScene(PhysXSceneContext::getSingleton().m_PxScene.get());
+			}
+			actor->UpdateLod(ViewedPos, TargetPos);
+		}
+	};
+
+	CPoint new_level_id = level_id + offset;
+	if (new_level_id.x >= 0 && new_level_id.x < m_Dim && new_level_id.y >= 0 && new_level_id.y < m_Dim)
+	{
+		const Vector3 InExtent(1000, 1000, 1000);
+		Vector3 LocalPos = TargetPos + Vector3(offset.x * 512.0f, 0, offset.y * 512.0f);
+		AABB InBox(LocalPos - InExtent, LocalPos + InExtent);
+		m_levels[new_level_id.y * m_Dim + new_level_id.x].QueryActor(InBox, &CallBack(this, ViewedPos, TargetPos));
+	}
 }
 
 void WorldL::ResetViewedActors(const CPoint & level_id, const my::Vector3 & ViewedPos, const my::Vector3 & TargetPos)
 {
+	const Vector3 OutExtent(1050, 1050, 1050);
+	AABB OutBox(TargetPos - OutExtent, TargetPos + OutExtent);
+	OctActorSet::iterator cmp_iter = m_ViewedActors.begin();
+	for (; cmp_iter != m_ViewedActors.end(); )
+	{
+		// ! all components world should be updated according to level_id
+		if (IntersectionTests::IntersectionTypeOutside
+			== IntersectionTests::IntersectAABBAndAABB(OutBox, (*cmp_iter)->m_aabb.transform((*cmp_iter)->m_World)))
+		{
+			if ((*cmp_iter)->IsRequested())
+			{
+				(*cmp_iter)->ReleaseResource();
+			}
+			(*cmp_iter)->OnLeavePxScene(PhysXSceneContext::getSingleton().m_PxScene.get());
+			cmp_iter = m_ViewedActors.erase(cmp_iter);
+		}
+		else
+			cmp_iter++;
+	}
+
+	_ResetViewedActors(level_id, CPoint(-1, -1), ViewedPos, TargetPos);
+	_ResetViewedActors(level_id, CPoint( 0, -1), ViewedPos, TargetPos);
+	_ResetViewedActors(level_id, CPoint( 1, -1), ViewedPos, TargetPos);
+
+	_ResetViewedActors(level_id, CPoint(-1,  0), ViewedPos, TargetPos);
+	_ResetViewedActors(level_id, CPoint( 0,  0), ViewedPos, TargetPos);
+	_ResetViewedActors(level_id, CPoint( 1,  0), ViewedPos, TargetPos);
+
+	_ResetViewedActors(level_id, CPoint(-1,  1), ViewedPos, TargetPos);
+	_ResetViewedActors(level_id, CPoint( 0,  1), ViewedPos, TargetPos);
+	_ResetViewedActors(level_id, CPoint( 1,  1), ViewedPos, TargetPos);
 }
