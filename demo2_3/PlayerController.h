f97c6b3fee128a232e50d768ef02a4a228424ea6
tangyin025 <tangyin025@126.com>
2017-06-03 18:03:16 +0800

    demo2_3 PlayerController

---
+++ demo2_3/PlayerController.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../myd3dbox/component/character.h"
+
+class PlayerController
+{
+public:
+	PlayerController(void);
+
+	virtual ~PlayerController(void);
+
+	void OnMouseMove(my::InputEventArg * arg);
+
+	void OnMouseBtnDown(my::InputEventArg * arg);
+
+	void OnMouseBtnUp(my::InputEventArg * arg);
+
+	void OnKeyDown(my::InputEventArg * arg);
+
+	void OnKeyUp(my::InputEventArg * arg);
+
+	void OnJoystickAxisMove(my::InputEventArg * arg);
+
+	void OnJoystickPovMove(my::InputEventArg * arg);
+
+	void OnJoystickBtnDown(my::InputEventArg * arg);
+
+	void OnJoystickBtnUp(my::InputEventArg * arg);
+};
