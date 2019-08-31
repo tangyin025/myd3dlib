2b6e241013f9cb157d4221a3258528c1feb49d10
tangyin025 <tangyin025@126.com>
2019-08-31 21:32:04 +0800

    revert ogreExporter.mll to maya2009

--- MayaExport/RunMaya.bat
+++ MayaExport/RunMaya.bat
@@ -1,3 +1,3 @@
 set MAYA_PLUG_IN_PATH=%MAYA_PLUG_IN_PATH%;%cd%\..\x64\Release
 set MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;%cd%\mel
-"C:\Program Files\Autodesk\Maya2019\bin\maya.exe"
+"C:\Program Files\Autodesk\Maya2009\bin\maya.exe"
