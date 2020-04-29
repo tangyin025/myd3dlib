acd3ce0f48771f89671bce4589eacba4e928cd80
tangyin025 <tangyin025@126.com>
2020-04-29 20:27:37 +0800

    ActionTrackAnimation

--- myengine/include/ActionTrack.h
+++ myengine/include/ActionTrack.h
@@ -1,5 +1,38 @@
 #pragma once
 
+#include <boost/shared_ptr.hpp>
+#include <vector>
+#include <map>
+
+class ActionTrack;
+
+typedef boost::shared_ptr<ActionTrack> ActionTrackPtr;
+
+class Action
+{
+public:
+	typedef std::vector<ActionTrackPtr> ActionTrackPtrList;
+
+	ActionTrackPtrList m_TrackList;
+
+public:
+	Action(void)
+	{
+	}
+
+	virtual ~Action(void)
+	{
+	}
+
+	void AddTrack(ActionTrackPtr track);
+
+	void RemoveTrack(ActionTrackPtr track);
+};
+
+class ActionTrackInst;
+
+typedef boost::shared_ptr<ActionTrackInst> ActionTrackInstPtr;
+
 class ActionTrack
 {
 public:
@@ -10,4 +43,72 @@ public:
 	virtual ~ActionTrack(void)
 	{
 	}
+
+	virtual ActionTrackInstPtr CreateInstance(void) const = 0;
+};
+
+class ActionTrackInst
+{
+public:
+	ActionTrackInst(void)
+	{
+	}
+
+	virtual ~ActionTrackInst(void)
+	{
+	}
+
+	virtual void Update(float fElapsedTime) = 0;
+};
+
+class Actor;
+
+class ActionTrackAnimation : public ActionTrack
+{
+public:
+	struct KeyFrame
+	{
+		std::string Name;
+		std::string RootList;
+		float Rate;
+		float BlendTime;
+		float BlendOutTime;
+		bool Loop;
+		int Prority;
+		float StartTime;
+		std::string Group;
+	};
+
+	typedef std::map<float, KeyFrame> KeyFrameMap;
+
+	KeyFrameMap m_Keys;
+
+public:
+	ActionTrackAnimation(void)
+	{
+	}
+
+	virtual ActionTrackInstPtr CreateInstance(Actor * actor) const;
+
+	void AddKeyFrame(float time, const char * Name, const char * RootList, float Rate, float BlendTime, float BlendOutTime, bool Loop, int Prority, float StartTime, const char * Group);
+};
+
+class ActionTrackAnimationInst : public ActionTrackInst
+{
+protected:
+	const ActionTrackAnimation * m_Template;
+
+	Actor * m_Actor;
+
+	float m_Time;
+
+public:
+	ActionTrackAnimationInst(const ActionTrackAnimation * Template, Actor * Actor)
+		: m_Template(Template)
+		, m_Actor(Actor)
+		, m_Time(0)
+	{
+	}
+
+	virtual void Update(float fElapsedTime);
 };
--- myengine/src/ActionTrack.cpp
+++ myengine/src/ActionTrack.cpp
@@ -1,1 +1,63 @@
 #include "ActionTrack.h"
+#include "Actor.h"
+#include "Animation.h"
+
+void Action::AddTrack(ActionTrackPtr track)
+{
+	m_TrackList.push_back(track);
+}
+
+void Action::RemoveTrack(ActionTrackPtr track)
+{
+	ActionTrackPtrList::iterator track_iter = std::find(m_TrackList.begin(), m_TrackList.end(), track);
+	if (track_iter != m_TrackList.end())
+	{
+		m_TrackList.erase(track_iter);
+	}
+}
+
+ActionTrackInstPtr ActionTrackAnimation::CreateInstance(Actor * actor) const
+{
+	return ActionTrackInstPtr(new ActionTrackAnimationInst(this, actor));
+}
+
+void ActionTrackAnimation::AddKeyFrame(float time, const char * Name, const char * RootList, float Rate, float BlendTime, float BlendOutTime, bool Loop, int Prority, float StartTime, const char * Group)
+{
+	KeyFrame & key = m_Keys[time];
+	key.Name = Name;
+	key.RootList = RootList;
+	key.Rate = Rate;
+	key.BlendTime = BlendTime;
+	key.BlendOutTime = BlendOutTime;
+	key.Loop = Loop;
+	key.Prority = Prority;
+	key.StartTime = StartTime;
+	key.Group = Group;
+}
+
+void ActionTrackAnimationInst::Update(float fElapsedTime)
+{
+	_ASSERT(m_Template);
+
+	_ASSERT(m_Actor);
+
+	ActionTrackAnimation::KeyFrameMap::const_iterator key_iter = m_Template->m_Keys.lower_bound(m_Time);
+	ActionTrackAnimation::KeyFrameMap::const_iterator key_end = m_Template->m_Keys.upper_bound(m_Time + fElapsedTime);
+	m_Time += fElapsedTime;
+	for (; key_iter != key_end; key_iter++)
+	{
+		if (m_Actor->m_Animation)
+		{
+			m_Actor->m_Animation->Play(
+				key_iter->second.Name,
+				key_iter->second.RootList,
+				key_iter->second.Rate,
+				key_iter->second.BlendTime,
+				key_iter->second.BlendOutTime,
+				key_iter->second.Loop,
+				key_iter->second.Prority,
+				key_iter->second.StartTime,
+				key_iter->second.Group);
+		}
+	}
+}
