62eed188c50cfdcbb5ab387c2954ed13cf36b6d2
tangyin025 <tangyin025@126.com>
2021-12-09 14:26:26 +0800

    SoundContext::BufferEventPairArray

--- myengine/include/SoundContext.h
+++ myengine/include/SoundContext.h
@@ -2,6 +2,11 @@
 
 #include "mySingleton.h"
 #include "mySound.h"
+#include <boost/array.hpp>
+
+class SoundEvent;
+
+typedef boost::shared_ptr<SoundEvent> SoundEventPtr;
 
 class SoundContext
 	: public my::SingleInstance<SoundContext>
@@ -11,6 +16,11 @@ public:
 
 	my::Sound3DListenerPtr m_listener;
 
+	typedef std::pair<my::SoundBufferPtr, SoundEventPtr> BufferEventPair;
+
+	typedef boost::array<BufferEventPair, 32> BufferEventPairArray;
+
+public:
 	SoundContext(void)
 	{
 	}
--- myengine/src/SoundContext.cpp
+++ myengine/src/SoundContext.cpp
@@ -9,7 +9,6 @@ bool SoundContext::Init(void)
 	DSBUFFERDESC             dsbd;
 	LPDIRECTSOUNDBUFFER      lpdsbPrimary;  // Cannot be IDirectSoundBuffer8.
 
-	HRESULT hr;
 	ZeroMemory(&dsbd, sizeof(DSBUFFERDESC));
 	dsbd.dwSize = sizeof(DSBUFFERDESC);
 	dsbd.dwFlags = DSBCAPS_CTRL3D | DSBCAPS_PRIMARYBUFFER;
