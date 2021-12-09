b3a4319392d8a1848fb180b8f0067ac2ddcfb7d3
tangyin025 <tangyin025@126.com>
2021-12-09 13:37:40 +0800

    SoundContext::Init, m_listener

--- myengine/src/SoundContext.cpp
+++ myengine/src/SoundContext.cpp
@@ -1,11 +1,28 @@
 #include "SoundContext.h"
 
+using namespace my;
+
 bool SoundContext::Init(void)
 {
+	m_sound.CreateSound();
+
+	DSBUFFERDESC             dsbd;
+	LPDIRECTSOUNDBUFFER      lpdsbPrimary;  // Cannot be IDirectSoundBuffer8.
+
+	HRESULT hr;
+	ZeroMemory(&dsbd, sizeof(DSBUFFERDESC));
+	dsbd.dwSize = sizeof(DSBUFFERDESC);
+	dsbd.dwFlags = DSBCAPS_CTRL3D | DSBCAPS_PRIMARYBUFFER;
+
+	m_sound.CreateSoundBuffer(&dsbd, &lpdsbPrimary);
+	SoundBuffer buff;
+	buff.Create(lpdsbPrimary);
+	m_listener = buff.Get3DListener();
+
 	return true;
 }
 
 void SoundContext::Shutdown(void)
 {
-
+	m_listener.reset();
 }
