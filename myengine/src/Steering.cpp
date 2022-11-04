4c3f7bc9b1ae49c07c0b7b223c8b7cba85f39bb5
tangyin025 <tangyin025@126.com>
2022-11-04 13:28:18 +0800

    Steering::SeekTarget, Separation

--- myengine/src/Steering.cpp
+++ myengine/src/Steering.cpp
@@ -323,44 +323,47 @@ Steering::CrowdAgentState Steering::SeekTarget(const my::Vector3& Target, float 
 	Vector3 dvel = (*(Vector3*)&m_cornerVerts[0] - m_agentPos).normalize() * m_MaxSpeed;
 
 	// Separation
-	const float separationDist = collisionQueryRange;
-	const float invSeparationDist = 1.0f / separationDist;
-	const float separationWeight = 8.0f;
-
-	float w = 0;
-	float disp[3] = { 0,0,0 };
-
-	for (int i = 0; i < neighbors.size(); ++i)
+	if (!(m_cornerFlags[m_ncorners - 1] & DT_STRAIGHTPATH_OFFMESH_CONNECTION) || dtVdist2DSqr(&m_cornerVerts[(m_ncorners - 1) * 3], &pos.x) > collisionQueryRange * collisionQueryRange)
 	{
-		const Steering* nei = neighbors[i];
-		const Controller* neicontroller = nei_controllers[i];
+		const float separationDist = collisionQueryRange;
+		const float invSeparationDist = 1.0f / separationDist;
+		const float separationWeight = 8.0f;
 
-		float diff[3];
-		Vector3 neipos = neicontroller->GetPosition();
-		dtVsub(diff, &m_agentPos.x, &neipos.x);
-		diff[1] = 0;
+		float w = 0;
+		float disp[3] = { 0,0,0 };
 
-		const float distSqr = dtVlenSqr(diff);
-		if (distSqr < 0.00001f)
-			continue;
-		if (distSqr > dtSqr(separationDist))
-			continue;
-		const float dist = dtMathSqrtf(distSqr);
-		const float weight = separationWeight * (1.0f - dtSqr(dist * invSeparationDist));
+		for (int i = 0; i < neighbors.size(); ++i)
+		{
+			const Steering* nei = neighbors[i];
+			const Controller* neicontroller = nei_controllers[i];
 
-		dtVmad(disp, disp, diff, weight / dist);
-		w += 1.0f;
-	}
+			float diff[3];
+			Vector3 neipos = neicontroller->GetPosition();
+			dtVsub(diff, &m_agentPos.x, &neipos.x);
+			diff[1] = 0;
 
-	if (w > 0.0001f)
-	{
-		// Adjust desired velocity.
-		dtVmad(&dvel.x, &dvel.x, disp, 1.0f / w);
-		// Clamp desired velocity to desired speed.
-		const float speedSqr = dtVlenSqr(&dvel.x);
-		const float desiredSqr = dtSqr(m_MaxSpeed);
-		if (speedSqr > desiredSqr)
-			dtVscale(&dvel.x, &dvel.x, desiredSqr / speedSqr);
+			const float distSqr = dtVlenSqr(diff);
+			if (distSqr < 0.00001f)
+				continue;
+			if (distSqr > dtSqr(separationDist))
+				continue;
+			const float dist = dtMathSqrtf(distSqr);
+			const float weight = separationWeight * (1.0f - dtSqr(dist * invSeparationDist));
+
+			dtVmad(disp, disp, diff, weight / dist);
+			w += 1.0f;
+		}
+
+		if (w > 0.0001f)
+		{
+			// Adjust desired velocity.
+			dtVmad(&dvel.x, &dvel.x, disp, 1.0f / w);
+			// Clamp desired velocity to desired speed.
+			const float speedSqr = dtVlenSqr(&dvel.x);
+			const float desiredSqr = dtSqr(m_MaxSpeed);
+			if (speedSqr > desiredSqr)
+				dtVscale(&dvel.x, &dvel.x, desiredSqr / speedSqr);
+		}
 	}
 
 	// Update the collision boundary after certain distance has been passed or
@@ -405,7 +408,7 @@ Steering::CrowdAgentState Steering::SeekTarget(const my::Vector3& Target, float 
 	params.adaptiveRings = 2;
 	params.adaptiveDepth = 5;
 	Vector3 vel = m_Forward * m_Speed;
-	ObstacleAvoidanceContext::getSingleton().sampleVelocityAdaptive(&m_agentPos.x, controller->GetRadius(), m_MaxSpeed,
+	ObstacleAvoidanceContext::getSingleton().sampleVelocityAdaptive(&m_agentPos.x, controller->GetRadius() - 0.01f, m_MaxSpeed,
 		&vel.x, &dvel.x, &nvel.x, &params, NULL);
 
 	// Integrate.
