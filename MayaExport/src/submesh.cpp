7cef173b5fc94341ffc5230b65f806f1c3b2ce53
tangyin025 <tangyin025@126.com>
2022-11-29 22:54:31 +0800

    Submesh::load, use shared vtx idx for non-shared geom

--- MayaExport/src/submesh.cpp
+++ MayaExport/src/submesh.cpp
@@ -164,12 +164,54 @@ namespace OgreMayaExporter
 						newTexCoords.w = 0;
 						v.texcoords.push_back(newTexCoords);
 					}
-					// add newly created vertex to vertex list
-					m_vertices.push_back(v);
+					int idx;
+					bool different = true;
+					// check if a vertex with same attributes has been saved already
+					for (int k = 0; k < m_vertices.size() && different; k++)
+					{
+						different = false;
+						if (m_vertices[k].x != v.x || m_vertices[k].y != v.y || m_vertices[k].z != v.z)
+						{
+							different = true;
+						}
+
+						if (params.exportVertNorm)
+						{
+							if (m_vertices[k].n.x != v.n.x || m_vertices[k].n.y != v.n.y || m_vertices[k].n.z != v.n.z)
+							{
+								different = true;
+							}
+						}
+
+						if ((params.exportVertCol) &&
+							(m_vertices[k].r != v.r || m_vertices[k].g != v.g || m_vertices[k].b != v.b || m_vertices[k].a != v.a))
+						{
+							different = true;
+						}
+
+						if (params.exportTexCoord)
+						{
+							for (int j = 0; j < vInfo.u.size(); j++)
+							{
+								if (m_vertices[k].texcoords[j].u != v.texcoords[j].u || m_vertices[k].texcoords[j].v != v.texcoords[j].v)
+								{
+									different = true;
+								}
+							}
+						}
+						idx = k;
+					}
+					// if no identical vertex has been saved, then save the vertex info
+					if (different)
+					{
+						// add newly created vertex to vertex list
+						m_vertices.push_back(v);
+						idx = m_vertices.size() - 1;
+					}
 					if (opposite)	// reverse order of face vertices to get correct culling
-						newFace.v[2-j] = m_vertices.size() - 1;
+						newFace.v[2-j] = idx;
 					else
-						newFace.v[j] = m_vertices.size() - 1;
+						newFace.v[j] = idx;
 				}
 			}
 			m_faces.push_back(newFace);
