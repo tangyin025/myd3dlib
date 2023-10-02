245093e46e308b6ef0f0bf0d898799ca1511c947
tangyin025 <tangyin025@126.com>
2023-10-02 11:26:18 +0800

    default_player_mesh_list, character/body.mesh.xml, body.skeleton.xml, thigh.L, thigh.R

--- myd3dbox/PlayerAgent.cpp
+++ myd3dbox/PlayerAgent.cpp
@@ -104,10 +104,10 @@ void PlayerAgent::RequestResource(void)
 	VERIFY(m_Animator = m_Actor->GetFirstComponent<Animator>());
 
 	AnimationNodePtr node_run_blend_list(new NodeRunBlendList("node_run_blend_list", this));
-	node_run_blend_list->SetChild(0, AnimationNodePtr(new AnimationNodeSequence("clip_drop")));
-	node_run_blend_list->SetChild(1, AnimationNodePtr(new AnimationNodeSequence("clip_stand")));
-	node_run_blend_list->SetChild(2, AnimationNodePtr(new AnimationNodeSequence("clip_run", 1.0f, true, "move")));
-	node_run_blend_list->SetChild(3, AnimationNodePtr(new AnimationNodeSequence("clip_run", 1.0f, true, "move")));
+	node_run_blend_list->SetChild(0, AnimationNodePtr(new AnimationNodeSequence("drop")));
+	node_run_blend_list->SetChild(1, AnimationNodePtr(new AnimationNodeSequence("stand")));
+	node_run_blend_list->SetChild(2, AnimationNodePtr(new AnimationNodeSequence("run", 1.0f, true, "move")));
+	node_run_blend_list->SetChild(3, AnimationNodePtr(new AnimationNodeSequence("run", 1.0f, true, "move")));
 
 	AnimationNodeSlotPtr node_run_blend_list_slot(new AnimationNodeSlot("node_run_blend_list_slot"));
 	node_run_blend_list_slot->SetChild(0, node_run_blend_list);
