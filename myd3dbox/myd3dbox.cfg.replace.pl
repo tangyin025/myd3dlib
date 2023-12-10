18094e2275c2643848ebab5195e2ffbaf7326e6d
tangyin025 <tangyin025@126.com>
2023-12-10 19:56:37 +0800

    default_checkbox_img_rect, default_checkbox_img_border

--- myd3dbox/myd3dbox.cfg.replace.pl
+++ myd3dbox/myd3dbox.cfg.replace.pl
@@ -1,10 +1,10 @@
 import re
 
 def repl(m):
-	l,t,w,h=int(m.group(1)),int(m.group(2)),int(m.group(3)),int(m.group(4))
-	return "_rect=%d,%d,%d,%d"%(l,t,l+w,t+h)
+	h,l,t,r,b=m.group(1),int(m.group(2)),int(m.group(3)),int(m.group(4)),int(m.group(5))
+	return "%s=%d,%d,%d,%d"%(h,l,t,r-1,b-1)
 
 a=open('myd3dbox.cfg','r')
 b=open('aaa.txt','w')
 for l in a.readlines():
-	b.write(re.sub("_rect=(\d+),(\d+),(\d+),(\d+)",repl,l))
+	b.write(re.sub("(checkbox_\w+_rect)=(\d+),(\d+),(\d+),(\d+)",repl,l))
