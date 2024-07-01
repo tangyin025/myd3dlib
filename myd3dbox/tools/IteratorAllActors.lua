70352295448f13b351ff0a060a7eaf1b1ad7505a
tangyin025 <tangyin025@126.com>
2024-07-01 09:46:51 +0800

    export Material::RequestResource, ReleaseResource

--- myd3dbox/tools/IteratorAllActors.lua
+++ myd3dbox/tools/IteratorAllActors.lua
@@ -1,3 +1,8 @@
 for act in theApp.MainWnd.allactors do
-	print(act.Name)
+	for cmp in act.Cmps do
+		local mat=cmp.Material
+		if mat and string.find(mat.Shader,"mtl_BlinnPhone.fx") then
+			print(mat.Shader)
+		end
+	end
 end
