d66297ee90a35a3069da87f3617ed6372a29cf66
tangyin025 <tangyin025@126.com>
2024-07-01 15:44:28 +0800

    tools/IteratorAllActors.lua, ParseShaderParameters

--- myd3dbox/tools/IteratorAllActors.lua
+++ myd3dbox/tools/IteratorAllActors.lua
@@ -1,8 +1,11 @@
 for act in theApp.MainWnd.allactors do
 	for cmp in act.Cmps do
 		local mat=cmp.Material
-		if mat and string.find(mat.Shader,"mtl_BlinnPhone.fx") then
+		if mat and string.find(mat.Shader,"mtl_BlinnPhong.fx") then
 			print(mat.Shader)
+			mat:ReleaseResource()
+			mat:ParseShaderParameters()
+			mat:RequestResource()
 		end
 	end
 end
