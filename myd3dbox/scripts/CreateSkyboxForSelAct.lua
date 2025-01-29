515c4b13a8ecf624463180bf76cf9c4bb570ec1f
tangyin025 <tangyin025@126.com>
2025-01-29 12:24:42 +0800

    Cube6Plane.mesh.xml, skybox_tex_path

--- myd3dbox/scripts/CreateSkyboxForSelAct.lua
+++ myd3dbox/scripts/CreateSkyboxForSelAct.lua
@@ -1,24 +1,24 @@
 skybox_tex_path={
 	"texture/cloudy_noon_FR.jpg",
-	"texture/cloudy_noon_UP.jpg",
 	"texture/cloudy_noon_BK.jpg",
-	"texture/cloudy_noon_DN.jpg",
-	"texture/cloudy_noon_RT.jpg",
 	"texture/cloudy_noon_LF.jpg",
+	"texture/cloudy_noon_RT.jpg",
+	"texture/cloudy_noon_UP.jpg",
+	"texture/cloudy_noon_DN.jpg",
 }
 for act in theApp.MainWnd.selactors do
 	print(os.date("%c"),"add sky box for "..act.Name)
-	for i=0,5,1 do
+	for i=1,6 do
 		-- print(i)
 		local mesh_cmp=MeshComponent(NamedObject.MakeUniqueName(act.Name.."_sky"))
 		mesh_cmp.MeshPath="mesh/Cube6Plane.mesh.xml"
-		mesh_cmp.MeshSubMeshId=i
+		mesh_cmp.MeshSubMeshId=i-1
 		mesh_cmp.Material=Material()
 		mesh_cmp.Material.Shader="shader/mtl_Skybox.fx"
 		mesh_cmp.Material:ParseShaderParameters()
 		mesh_cmp.Material.CullMode=Material.D3DCULL_CCW
 		mesh_cmp.Material.ZEnable=false
-		mesh_cmp.Material:SetParameter("g_DiffuseTexture",skybox_tex_path[i+1])
+		mesh_cmp.Material:SetParameter("g_DiffuseTexture",skybox_tex_path[i])
 		act:InsertComponent(mesh_cmp)
 	end
 	break
